d25467a525c0467aa06ff5a7d335c2f82c062aa5
Onat Yiğit Mercan <onatmercan@gmail.com>
2014-03-25 01:50:47 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# AK 2048 Android
+Bir [2048](https://github.com/gabrielecirulli/2048) klonudur.
+
+[![Buradan oyna!](https://developer.android.com/images/brand/en_generic_rgb_wo_60.png)](https://play.google.com/store/apps/details?id=org.ak2048.game)
+
+### Screenshot
+
+[![Screenshot](http://i.imgur.com/i8GU9zE.png)
+
+## Lisans
+AK 2048 [MIT lisansı] ile lisanslanmıştır. (https://github.com/onatm/ak2048/blob/master/LICENSE)
---
+++ src/org/ak2048/game/MainActivity.java
@@ -0,0 +1,19 @@
+package org.ak2048.game;
+
+import org.apache.cordova.Config;
+import org.apache.cordova.CordovaActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends CordovaActivity 
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.init();
+        // Set by <content src="index.html" /> in config.xml
+        super.loadUrl(Config.getStartUrl());
+        //super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
