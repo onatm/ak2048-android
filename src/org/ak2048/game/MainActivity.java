b4a84ca906b732b15614d7d9a8dd9854601b69a8
Onat Yiğit Mercan <onatmercan@gmail.com>
2014-04-02 16:36:24 +0300

    admob removed and new icon added.

--- src/org/ak2048/game/MainActivity.java
+++ src/org/ak2048/game/MainActivity.java
@@ -2,30 +2,17 @@ package org.ak2048.game;
 
 import org.apache.cordova.Config;
 import org.apache.cordova.CordovaActivity;
-import com.google.ads.*;
-import android.widget.LinearLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends CordovaActivity 
 {
-	private static final String AdMob_Ad_Unit = "AdMob_Ad_Unit";
-	private AdView adView;
-	
-    @SuppressWarnings("deprecation")
-	@Override
+    @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         super.init();
 
         super.loadUrl(Config.getStartUrl());
-
-        adView = new AdView(this, AdSize.BANNER, AdMob_Ad_Unit); 
-        LinearLayout layout = super.root;
-        layout.addView(adView); 
-        AdRequest request = new AdRequest();
-        request.setTesting(false);
-        adView.loadAd(request);
     }
 }
