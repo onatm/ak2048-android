e0c56b798a786b20465fe6d2c04c06e36645a1e1
Onat Yiğit Mercan <onatmercan@gmail.com>
2014-04-21 20:36:17 +0300

    admob added again.

--- src/org/ak2048/game/MainActivity.java
+++ src/org/ak2048/game/MainActivity.java
@@ -2,17 +2,30 @@ package org.ak2048.game;
 
 import org.apache.cordova.Config;
 import org.apache.cordova.CordovaActivity;
+import com.google.ads.*;
+import android.widget.LinearLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends CordovaActivity 
 {
-    @Override
+	private static final String AdMob_Ad_Unit = "ca-app-pub-4231817588171293/4917918965";
+	private AdView adView;
+	
+    @SuppressWarnings("deprecation")
+	@Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         super.init();
 
         super.loadUrl(Config.getStartUrl());
+
+        adView = new AdView(this, AdSize.BANNER, AdMob_Ad_Unit); 
+        LinearLayout layout = super.root;
+        layout.addView(adView); 
+        AdRequest request = new AdRequest();
+        request.setTesting(false);
+        adView.loadAd(request);
     }
 }
